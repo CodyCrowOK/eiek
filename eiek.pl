0e547c60f75e9bed72a362f6cc2ddccd83c1e71f
Cody Crow <cody@cms07.org>
2014-11-13 10:19:35 -0600

    Working add, clear, and commit.

--- eiek.pl
+++ eiek.pl
@@ -4,14 +4,49 @@ use strict;
 use warnings;
 use feature ":5.10";
 use Tkx;
+use File::PathConvert qw(abs2rel);
 
 our $VERSION = "0.01";
+my $cwd;
 
 my $IS_AQUA = Tkx::tk_windowingsystem() eq "aqua";
 
 my $mw = Tkx::widget->new(".");
 
+#Create the menu bar
 $mw->configure(-menu => create_menu($mw));
+my $splash = $mw->new_ttk__frame(-padding => "3 3 12 12");
+my $content = $mw->new_ttk__frame(-padding => "3 3 12 12");
+
+#Prompt the user, asking if they'd like to load a repo or create a new one.
+my $loadrepo_button = $splash->new_button(
+	-text => "Load repository",
+	-command => sub {
+		my $dirname = Tkx::tk___chooseDirectory();
+		if ($dirname) {
+			$cwd = $dirname;
+			chdir $cwd;
+			&destroy_splash;
+		}
+	},
+);
+
+my $newrepo_button = $splash->new_button(
+	-text => "New repository",
+	-command => sub {
+		my $dirname = Tkx::tk___chooseDirectory();
+		if ($dirname) {
+			$cwd = $dirname;
+			chdir $cwd;
+			&eie_init;
+			&destroy_splash;
+		}
+	},
+);
+
+$splash->g_place(-relx => 0, -rely => 0, -relwidth => 1.0, -relheight => 1.0);
+$newrepo_button->g_place(-relx => 0, -rely => 0, -relwidth => 1.0, -relheight => .5);
+$loadrepo_button->g_place(-relx => 0, -rely => .5, -relwidth => 1.0, -relheight => .5);
 
 Tkx::wm_title($mw, "eiek");
 Tkx::wm_minsize($mw, 800, 600);
@@ -69,5 +104,106 @@ sub about {
 		-icon => "info",
 		-message => "eiek v$VERSION\n" .
 			"Licensed under GPLv2",
+	);
+}
+
+sub destroy_splash {
+	$splash->g_destroy();
+	&load_main;
+}
+
+sub load_main {
+	my $add_button = $content->new_button(
+		-text => "Add files to be committed",
+		-command => sub {
+			my $files = Tkx::tk___getOpenFile(
+				-title => "Add files",
+				-multiple => 1,
+			);
+			eie_add($files);
+		},
+	);
+	my $clear_button = $content->new_button(
+		-text => "Clear files to be committed",
+		-command => sub {
+			&eie_clear;
+		},
+	);
+	my $commit_button = $content->new_button(
+		-text => "Commit queued files",
+		-command => sub {
+			&eie_commit;
+		},
+	);
+	my $list_button = $content->new_button(
+		-text => "List all commits containing a particular file",
+		-command => sub {
+			
+		},
+	);
+	my $revert_button = $content->new_button(
+		-text => "Revert a file to its contents at a particular commit",
+		-command => sub {
+			
+		},
+	);
+	my $destroy_button = $content->new_button(
+		-text => "Destroy eie repository",
+		-command => sub {
+			
+		},
+	);
+	$content->g_place(-relx => 0, -rely => 0, -relwidth => 1.0, -relheight => 1.0);
+	$add_button->g_place(-relx => 0, -rely => 0, -relwidth => 1.0, -relheight => .1666666666667);
+	$clear_button->g_place(-relx => 0, -rely => .1666666666667, -relwidth => 1.0, -relheight => .1666666666667);
+	$commit_button->g_place(-relx => 0, -rely => .3333333333333, -relwidth => 1.0, -relheight => .1666666666667);
+	$list_button->g_place(-relx => 0, -rely => .5, -relwidth => 1.0, -relheight => .1666666666667);
+	$revert_button->g_place(-relx => 0, -rely => .6666666666667, -relwidth => 1.0, -relheight => .1666666666667);
+	$destroy_button->g_place(-relx => 0, -rely => .8333333333333, -relwidth => 1.0, -relheight => .1666666666667);
+
+}
+
+sub eie_init {
+	system ('eie', 'init');
+}
+
+sub eie_add {
+	my $i = 0;
+	while (@_) {
+		my $f = shift;
+		my @files = split(/ /, $f);
+		foreach my $file (@files) {
+			$i++;
+			system ('eie', 'add', abs2rel($file, $cwd));
+		}
+	}
+	Tkx::tk___messageBox(
+		-parent => $mw,
+		-title => "Add Files",
+		-type => "ok",
+		-icon => "info",
+		-message => "$i Files added successfully.",
+	);
+}
+
+sub eie_clear {
+	system ('eie', 'clear');
+	Tkx::tk___messageBox(
+		-parent => $mw,
+		-title => "Clear",
+		-type => "ok",
+		-icon => "info",
+		-message => "Queue cleared successfully.",
+	);
+}
+
+sub eie_commit {
+	system ('eie', 'commit');
+	Tkx::tk___messageBox(
+		-parent => $mw,
+		-title => "Commit",
+		-type => "ok",
+		-icon => "info",
+		-message => "All queued files committed successfully.",
 	);
 }
